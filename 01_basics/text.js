bbd32bd91b4709b3bc3bc7b93ad534fda4b39d82
Shiva Maurya <127747702+reactshiva-135790@users.noreply.github.com>
2023-07-21 15:17:47 +0000

    data-types

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,32 @@
-console.log("Hello Shiva")
+console.log("Hello Shiva");
+
+var a = "a is not consonent";
+let b= "b is not volwel";
+const c = "c is consonent.";
+
+
+
+/* 
+var has issue with block and fucntion scope that's why let and const came in picture
+*/
+// console.table([a,b,c])
+
+/* 
+We have diffrent data types
+
+1.Number;
+2.boolean;
+3.BigInt;
+4.String;
+5.Undefined;
+6.Null;
+7.Symbol;
+
+
+*/
+
+
+
+
+
+
